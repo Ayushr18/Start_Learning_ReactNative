5bcc544cdc3aa78c91f140adb9ac135cc4947c5e
Ayushr18 <121632238+Ayushr18@users.noreply.github.com>
2024-04-11 11:44:43 +0530

    start weather application

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
+import React from "react";
+import {View, StyleSheet} from 'react-native'
+import CurrentWeather from "./SRC/screens/CurrentWeather";
+import UpcomingWeather from "./SRC/screens/UpcomingWeather";
+import City from "./SRC/screens/City";
+const App = () => {
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <City/>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+      flex:  1
+    }
+})
+export default App;
